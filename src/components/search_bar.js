b1e0be867b54a46dedae56229e18992db3a551c5
Rohan Mehta <Rohan.Mehta@apdcares.org>
2017-11-07 17:15:33 -0500

    handler added

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,8 +1,14 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 class SearchBar extends React.Component {
-render() {
-    return <input />;
+
+  render() {
+    return <input onChange = { this.onInputChange } />;
+  }
+
+  onInputChange(event) {
+    // console.log("event: " + event.target.value);
+    console.log(event);
   }
 }
 
