ed17050c21a7a1bbadbf827bb4cca396196f4436
Rohan Mehta <Rohan.Mehta@apdcares.org>
2017-11-07 11:51:48 -0500

    starting fresh index.js -- most basic

--- src/index.js
+++ src/index.js
@@ -1,15 +1,8 @@
+// Create a new compinent. This component should produce some HTML
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+const App = function(){
+  return <div> Hi! </div>;
+}
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+React.render(App);
