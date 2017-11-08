4a4f74262d4b818ab53a7679d8995a9558812756
Rohan Mehta <Rohan.Mehta@apdcares.org>
2017-11-08 11:44:05 -0500

    added more stuff to searchbar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,15 +1,41 @@
-import React, { Component } from 'react';
+import React, {
+  Component
+} from 'react';
 
 class SearchBar extends React.Component {
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      term: 'w'
+    };
+  }
+
   render() {
-    return <input onChange = { this.onInputChange } />;
+
+    var that = this;
+      return  ( <input value = {this.state.term} onChange = {
+        function(event){
+        that.onInputChange(event)
+      }
+    } /> );
+
+    // return (<input onChange = { event => this.setState({ term: event.target.value }) } /> );
   }
 
   onInputChange(event) {
     // console.log("event: " + event.target.value);
+
+    if (event) {
+      this.setState({
+        term: event.target.value
+      });
+    }
+
     console.log(event);
   }
+
 }
 
 export default SearchBar;
