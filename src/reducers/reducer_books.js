b80b9522d3843ca12c2a7414c599e428098e6235
Rohan Mehta <Rohan.Mehta@apdcares.org>
2017-11-13 11:25:22 -0500

    added 1st reducer and combiner

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,18 @@
+export default function() {
+  return [{
+      title: 't1'
+    },
+    {
+      title: 't2'
+    },
+    {
+      title: 't3'
+    },
+    {
+      title: 't4'
+    },
+    {
+      title: 't5'
+    },
+  ]
+}
