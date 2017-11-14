7dd640adb90f7edb9aee5d4fca99c153b1c0f7dd
Rohan Mehta <Rohan.Mehta@apdcares.org>
2017-11-14 11:16:33 -0500

    clean ups

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -3,32 +3,30 @@ import { connect } from 'react-redux';
 
 export default class BookList extends Component {
 
-  var bookItems = "";
-
-    renderList() {
-      return this.props.books.map((book) => {
-          return (
-              <li key="book.title" className = "list-group-item" >
-              { book.title }
-              < /li> );
-          })
-      }
-
-    render() {
-      return (
-        <ul className = "list-group col-sm-4" > {
-          this.renderList()
-        }
-        < BookItem >
-        < /BookItem>
-        </ul>
-      );
-    }
+  renderList() {
+    return this.props.books.map((book) => {
+        return (
+            <li key="book.title" className = "list-group-item" >
+            { book.title }
+            < /li> );
+        })
   }
+
+  render() {
+    return (
+      <ul className = "list-group col-sm-4" > {
+        this.renderList()
+      }
+      < BookItem >
+      < /BookItem>
+      </ul>
+    );
+  }
+}
 
 function mapStateToProps(state){
   return {
-    books: state.books;
+    books: state.books
   };
 }
 
