fcae83874014cf20e4273b1f92b68c224dedb644
Rohan Mehta <Rohan.Mehta@apdcares.org>
2017-11-16 11:10:15 -0500

    filtering request actions

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,4 +1,15 @@
-export default function(state = null, action){
+import FETCH_WEATHER from '../actions/index.js';
+
+export default function(state = [], action){
+
+  switch (action.type) {
+    case FETCH_WEATHER:
+      return state.concat([action.payload.data]);   // DO NOT MODIFY STATE BY PUSHING CITIES
+       //return [action.payload.data, ...state];   // this will concat new entries to top instead of bottom
+      break;
+    default:
+  }
+
   console.log('action received:', action);
   return state;
 }
