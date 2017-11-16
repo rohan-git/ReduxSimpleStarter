6ec745a0f6dd741f8146ff00947f91b1d34d9bda
Rohan Mehta <Rohan.Mehta@apdcares.org>
2017-11-16 12:08:12 -0500

    spelling

--- src/reducers/reducer-weather.js
+++ src/reducers/reducer-weather.js
@@ -4,7 +4,7 @@ export default function(state = [], action){
 
   switch (action.type) {
     case FETCH_WEATHER:
-      return state.concat([action.payload.data]);   // DO NOT MODIFY STATE BY PUSHING CITIES
+      return state.concat([action.payload.data]);   // DO NOT `PUSH` CITIES IT WILL MODIFY STATE
        //return [action.payload.data, ...state];   // this will concat new entries to top instead of bottom
       break;
     default:
