9264a3a0beec47c4406fce4220a08e3dcd300ab9
Rohan Mehta <Rohan.Mehta@apdcares.org>
2017-11-16 10:49:09 -0500

    fixed request url

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,12 +2,14 @@ import axios from 'axios';
 
 const API_KEY = '91e924f88d327fb06984955a693516eb';
 export const FETCH_WEATHER  = 'FETCH_WEATHER';
-export const ROOT_URL  =  `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}&q={}`;
+export const ROOT_URL  =  `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export function fetchWeather(city){
 
   const url = `${ROOT_URL}&q=${city},US`;
   const request = axios.get(url);
+
+  console.log('req:', request);
 
   return {
     type: FETCH_WEATHER,
