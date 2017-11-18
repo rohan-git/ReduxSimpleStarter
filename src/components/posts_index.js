42c8ee5347306621bef9394d72d0be98e202d589
rohan <rohan@MacBook-Pro-275.local>
2017-11-18 16:47:08 -0500

    created body for posts index

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,8 +1,57 @@
-import React, {Component} from 'react';
+import _ from 'lodash';
 
-export default class PostsIndex extends Component{
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import { fetchPosts } from '../actions/index.js';
+
+class PostsIndex extends Component {
+
+  componentDidMount(){
+
+    this.props.fetchPosts();
+
+    console.log('this', this);
+    console.log('prps', this.props);
+    console.log('componentDidMount', this.props.posts);
+
+  }
+
+  renderPosts(){
+
+    console.log('renderPosts', this.props.posts);
+
+    return _.map(this.props.posts,  post => {
+      return (<div key={post.id}>
+                <div>{post.id} </div>
+                <div>{post.title} </div>
+                <div>{post.categories} </div>
+                <div>{post.content} </div>
+              </div>);
+    });
+
+  }
 
   render(){
-    return (<div> Posts Index</div>);
+
+    console.log('postsRender', this.props.posts);
+
+    return (
+      <div>
+        <div> Posts Index </div>
+        {this.renderPosts()}
+      </div>
+    );
   }
 }
+
+function mapStateToProps(state){
+  return { posts: state.posts };
+}
+
+function mapDispactchToProps(dispatch) {
+  return bindActionCreators({ fetchPosts }, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispactchToProps)(PostsIndex);
