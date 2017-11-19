4238cc6efa42a87dfbeba20fd089394e64e47cfc
rohan <rohan@MacBook-Pro-275.local>
2017-11-19 18:40:37 -0500

    created dummy routes

--- src/index.js
+++ src/index.js
@@ -2,14 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route } from 'redux';
 
 import App from './components/app';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
+class Hello extends React.Component {
+
+  render(){
+    render(<div> hello ... </div>);
+  }
+
+class GoodBye extends React.Component {
+
+  render(){
+    render(<div> GoodBye ... </div>);
+  }
+
+}
+
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <App />
+    <BrowserRouter>
+      <Route path="/" component={} />
+
+      </Route>
+    </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
