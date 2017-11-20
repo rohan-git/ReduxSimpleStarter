5d62d1abf515ff2cc27b31a33f783b0348e1935c
Rohan Mehta <Rohan.Mehta@apdcares.org>
2017-11-20 14:51:52 -0500

    removed deletedPost from state too; easier with {} as parent state than []

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,11 +1,16 @@
 import _ from 'lodash';
 
-import { FETCH_POSTS, FETCH_POST } from '../actions';
+import { FETCH_POSTS, FETCH_POST, DELETE_POST } from '../actions';
 import { combineReducers } from 'redux';
 
 export default function(state = {}, action){
 
   switch (action.type) {
+
+    case DELETE_POST:
+        return _.omit(state, action.payload);
+
+      break;
 
     case FETCH_POST:
 
