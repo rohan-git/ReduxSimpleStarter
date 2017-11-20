ae95b5e9f60aedccfa12a71df7e52518deb54bf1
rohan <rohan@MacBook-Pro-275.local>
2017-11-19 23:31:01 -0500

    logic to create and render 1st field (title) in the form

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,8 +1,36 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 
-export default class PostsNew extends React.Component {
+class PostsNew extends React.Component {
+
+  renderTitleField(field){
+
+    return (
+      <div className="form-group">
+        <label className="input-group"> Title: </label>
+        <input className="form-control" type="text" {...field.input} />
+      </div>
+    );
+
+    // ...field.input instead of onChange={} onBlur={}, etc...
+    // takes care of all of them
+  }
+
   render(){
-    return (<div> ... </div>);
+    return (<div>
+              <h3> Add A New Post</h3>
+              <div className="col col-lg-6">
+              <form className="form form-inline">
+                <Field name="title" component={this.renderTitleField} />
+              </form>
+              </div>
+              <div className="col col-lg-6"> .. </div>
+            </div>
+          );
   }
 }
+
+export default reduxForm({
+  form: 'PostsNewForm' // ..just has to be a unique string
+
+})(PostsNew);
