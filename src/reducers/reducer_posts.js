eb0be94582416931ad130630eacfccb4f03d93d0
Rohan Mehta <Rohan.Mehta@apdcares.org>
2017-11-20 12:59:47 -0500

    fancy way to add new keyValue pair to a new state obj

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -7,22 +7,17 @@ export default function(state = {}, action){
 
   switch (action.type) {
 
-    case FETCH_POSTS:
-
-        console.log('action.payload.data', action.payload.data);
-
-        const post = action.payload.data;
-        const newState = { ...state };
-        newState[posts.id] = post;
-
-        return newState;
-        
-      break;
-
     case FETCH_POST:
 
         console.log('action.payload.data', action.payload.data);
-        return action.payload.data;
+        return { ...state, [action.payload.data.id]: action.payload.data};
+
+      break;
+
+    case FETCH_POSTS:
+
+        console.log('action.payload.data', action.payload.data);
+        return _.mapKeys(action.payload.data, 'id');
 
       break;
 
