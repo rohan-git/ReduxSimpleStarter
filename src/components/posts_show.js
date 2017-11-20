61f7113169b7e102c1aa17660b527fe45eb6e32b
Rohan Mehta <Rohan.Mehta@apdcares.org>
2017-11-20 14:46:50 -0500

    work done for deletePost

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -21,10 +21,13 @@ class PostsShow extends React.Component {
       }
   }
 
-  onDeleteClick(id){
+  onDeleteClick(){
 
     const { id }  = this.props.match.params;
+
     this.props.deletePost(id, () => {
+
+      console.log("post deleted");
       this.props.history.push("/");
     });
 
@@ -43,9 +46,9 @@ class PostsShow extends React.Component {
     return (
       <div class="container">
         <Link className="" to="/"> Back </Link>
-        <Link className="btn btn-danger pull-xs-right" onClick={onDeleteClick}>
+        <button className="btn btn-danger pull-xs-right" onClick={this.onDeleteClick.bind(this)}>
           Delete This Post
-        </Link>
+        </button>
         <h3> { post.title } </h3>
         <h6> Categories: { post.categories } </h6>
         <p> {post.content} </p>
